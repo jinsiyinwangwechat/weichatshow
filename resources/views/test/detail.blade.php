f0b0f5c4a9c8a0b1ee6f5a35c685a2d3bf24e8d5
刘成 <liucheng@yunniao.me>
2017-08-16 08:19:30 +0800

    去这里地图显示

--- resources/views/test/detail.blade.php
+++ resources/views/test/detail.blade.php
@@ -107,7 +107,7 @@
                 <a href="#" class="ui-btn ui-corner-all ui-btn-inline ui-mini footer-button-left ui-btn-icon-left ui-icon-phone">打电话</a>
             </div>
             <div class="ui-block-b" style="text-align: center">
-                <a href="#" class="ui-btn ui-corner-all ui-btn-inline ui-mini footer-button-left ui-btn-icon-left ui-icon-navigation">去这里</a>
+                <a href="http://m.amap.com/navi/?start=115.524137,38.242731&dest=115.519545,38.228472&destName=刘成测试地图选点&key=175cd59f097bc8498dd2e4e8f3868cd2" class="ui-btn ui-corner-all ui-btn-inline ui-mini footer-button-left ui-btn-icon-left ui-icon-navigation">去这里</a>
             </div>
             <div class="ui-block-c" style="text-align: right">
                 <a href="#" class="ui-btn ui-corner-all ui-btn-inline ui-mini footer-button-left ui-btn-icon-left ui-icon-star">收藏&nbsp;&nbsp;&nbsp;</a>
