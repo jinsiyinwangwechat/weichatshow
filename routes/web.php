eca7877a07c9391a4b63ed3538dba3e8d56c1c47
刘成 <liucheng@yunniao.me>
2017-08-16 08:02:32 +0800

    修复ajax访问

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,7 @@ Route::get('/', function () {
 
 Route::any('/wechat', 'WechatController@serve');
 Route::any('/factory/detail', 'FactoryController@getFactoryDetail');
-Route::get('/factory/listPage', 'FactoryController@listPage');
-Route::get('/factory/detailPage', 'FactoryController@detailPage');
+
 Route::get('/factory/list','FactoryController@getFactoryList');
 Route::get('/factory/search', 'FactoryController@searchFactory');
 Route::get('/factory/my', 'FactoryController@collectListPage');
@@ -28,4 +27,6 @@ Route::post('/cancel/collection', 'FactoryController@doCollection');
 Route::group([ 'middleware' => ['wechat.oauth']], function () {
 
     Route::any('/test', 'TestController@test');
+    Route::get('/factory/listPage', 'FactoryController@listPage');
+    Route::get('/factory/detailPage', 'FactoryController@detailPage');
 });
